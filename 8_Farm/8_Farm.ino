cc1ba6281d29730f002e8ae60f738171d6442083
ATOIsus <sabinmaharjan884@gmail.com>
2022-09-12 10:30:59 +0545

    Farm project data added.

--- 8_Farm/8_Farm.ino
+++ 8_Farm/8_Farm.ino
@@ -1,33 +1,48 @@
+#include <DHT.h>
 
-int relayMotor = 2;
-int relayLight = 3;
+int DHTPIN = D3;
+#define DHTTYPE   DHT22
 
-int dhtIn = 6;
-int dhtValue;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+int relayMotor = D0;
+int relayLight = D1;
+
+int dhtIn = D3;
+float dhtTemp;
+float dhtHumidity;
 
 int moistureIn = A0;
 int moistureValue;
 
 void setup() {
+  dht.begin();
+
+  Serial.begin(9600);
+
   pinMode(relayMotor, OUTPUT);
   pinMode(relayLight, OUTPUT);
 
-  pinMode(dhtIn, INPUT);
   pinMode(moistureIn, INPUT);
 }
 
 void loop() {
 
   moistureValue = analogRead(moistureIn);
-  dhtValue = digitalRead(dhtIn);
+
+  dhtHumidity = dht.readHumidity();
+  dhtTemp = dht.readTemperature();
+
+  Serial.println((String)"Moisture Value: " + moistureValue  + " ,DHT Temp:  " + dhtTemp + " ,DHT Humidity:  " + dhtHumidity);
 
   digitalWrite(relayMotor, HIGH);
   digitalWrite(relayLight, HIGH);
-  delay(5000);
+  delay(500);
 
 
   digitalWrite(relayMotor, LOW);
   digitalWrite(relayLight, LOW);
-  delay(5000);
+  delay(500);
 
 }
